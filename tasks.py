d6e02f21bd280bcc1ea1822ad4d7d9f0b6cc7efd
Alexander <sashok935@gmail.com>
2021-05-03 12:26:56 +0300

    Add cookies to avoid GDPR consent screen

--- tasks.py
+++ tasks.py
@@ -1,15 +1,23 @@
 import os
+from time import time
 from celery import Celery
 from chat_downloader import ChatDownloader
+from youtube_dl import YoutubeDL
 
-celery = Celery(__name__, broker='redis://localhost:6379', backend='redis://localhost:6379')
+celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
 
 
 @celery.task()
 def retrieve_chat(url):
-    chat = ChatDownloader().get_chat(url, message_types=['paid_message'])
+    # https://github.com/xenova/chat-downloader/issues/85#issuecomment-813662554
+    with YoutubeDL({'cookiefile': 'cookies.ytdl', 'skip_download': True}) as ytdl:
+        ytdl.download([url])
+    cookies = open('cookies.ytdl', 'r').read().replace('\t0\t', '\t{:.0f}\t'.format(time() + 60 * 60 * 24 * 90))
+    open('cookies.ytdl', 'w').write(cookies)
+
+    chat = ChatDownloader(cookies='cookies.ytdl').get_chat(url, message_types=['paid_message'])
     # TODO: insert empty chat check
     messages = []
     for message in chat:
