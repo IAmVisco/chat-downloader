ad046950b9c22996e34963cdd732172063809899
IAmVisco <IAmVisco@users.noreply.github.com>
2021-08-18 17:53:49 +0000

    deploy: aaa7121bccdfdae2f9ada3b3decc8ab035b648e6

--- README.md
+++
@@ -1,5 +0,0 @@
-# YouTube Chat Downloader
-
-Basically a web UI for [this CLI tool](https://github.com/xenova/chat-downloader)
-
-Very WIP
--- start-celery.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo service redis-server status > /dev/null || sudo service redis-server start
-celery -A tasks worker --loglevel=info
